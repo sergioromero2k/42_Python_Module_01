0737ae45cc355f0c2a2977f505eff9b9fd8e9131
Sergio Alejandro <sergioromero2k@gmail.com>
2025-12-24 21:22:11 +0100

    First part of the Python module of project 01

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,14 @@
+def ft_garden_intro():
+    plant_name = "Rose"
+    height = "25cm"
+    age = "30 days"
+
+    print("=== Welcome to My Garden ===")
+    print(f"Plant: {plant_name}")
+    print(f"Height: {height}")
+    print(f"Age: {age}")
+    print("=== End of Program ===")
+
+
+if __name__ == "__main__":
+    ft_garden_intro()
